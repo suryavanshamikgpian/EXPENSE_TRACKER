d64986d346cb18a0b0258be9869e80f7ed31d00d
deetosh <deetoshkuila18@gmail.com>
2025-01-18 15:14:48 +0530

    Revert "first commit"
    This reverts commit c8bbae5c1a19d5bcd689064eedacb292ce0dc9ec.

--- Readme.md
+++
@@ -1,5 +0,0 @@
-# Expense Tracker Application
-
-Members
-1. Deetosh Kumar Kuila - 22CS30021
-2. Aashray Tandon - 22CS30002
--- app.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-const dotenvResult = dotenv.config({ path: `.env.${process.env.ENVIRONMENT}` });
-if (dotenvResult.error) {
-  throw dotenvResult.error;
-}
